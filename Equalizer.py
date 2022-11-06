fd341e7829ad0a5f98d8ea3b8f909595162161c8
Your Name <marinanasser8@gmail.com>
2022-11-06 20:42:29 +0200

    spectrogram/csv

--- Equalizer.py
+++ Equalizer.py
@@ -20,9 +20,12 @@ if 'play_state' not in st.session_state:
 
 if 'uploaded' not in st.session_state:
      st.session_state['uploaded']= False
+
+if 'slider' not in st.session_state:
+     st.session_state['slider']= 0
 #------------------------------------------------------------------Upload_file----------------------------------------------------------------------------------------------------------------------------------------------
 
-uploaded_file = st.file_uploader("uploader",key="uploaded_file",label_visibility="hidden")
+uploaded_file = st.sidebar.file_uploader("uploader",key="uploaded_file",label_visibility="hidden")
 
 if uploaded_file is not None:
     st.session_state['uploaded']= True
@@ -30,73 +33,32 @@ if uploaded_file is not None:
     ext = os.path.splitext(file_name)[1][1:]
     # st.write(ext)
 
-
+    data=[]
+#------------------------------------------------------------------csv----------------------------------------------------------------------------------------------------------------------------------------------
     if ext=='csv':
         df = pd.read_csv(uploaded_file)
         list_of_columns=df.columns
         x_axis = df[list_of_columns[0]].to_numpy()
-        y_axis = df[list_of_columns[1]].to_numpy()
-        functions.show_signal(x_axis,y_axis)
+        data = df[list_of_columns[1]].to_numpy()
+        functions.show_signal(x_axis,data)
+        max_freq=functions.getFMax(x_axis,data)
+        nequist_freq=2*max_freq
+       
 
 
+#------------------------------------------------------------------wav----------------------------------------------------------------------------------------------------------------------------------------------
     elif ext=='wav':
         # functions.Audio_player(uploaded_file)
         data, samplerate  = functions.handle_uploaded_audio_file(uploaded_file)
         duration = len(data)/samplerate
         time = np.arange(0,duration,1/samplerate)
-        fft_sig, amplitude,phase,frequency=functions.Fourier_transform(data,samplerate)
-        freq_axis_list, amplitude_axis_list,bin_max_frequency_value=functions.bins_separation(frequency, amplitude)
-        #sliders_date=functions.generate_sliders(bin_max_frequency_value)
-        # st.write(bin_max_frequency_value)
-        sliders_data=functions.generate_sliders(bin_max_frequency_value)
-        # mod_amplitude_axis_list,empty=functions.sound_modification(sliders_data,amplitude_axis_list)
-        # phase=phase[:len(mod_amplitude_axis_list):1]
-        # ifft_file=functions.inverse_fourier(mod_amplitude_axis_list,phase)    # generate = st.button('Generate')
-        # uploaded_file=ipd.Audio(ifft_file,rate=samplerate/2)
-        # empty.write(uploaded_file)
-        # frequency=frequency[:len(mod_amplitude_axis_list):1]
-
-
-
-
-        # st.write("Duration of Audio in Seconds", duration)
-        # st.write("Duration of Audio in Minutes", duration/60)
-        functions.show_signal(time,data)
-
-#-----------------------------------------------------Play Button-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    st.button(label= "DISPLAY" ,#if not st.session_state["play_state"] else "PAUSE", 
-    disabled= not st.session_state['uploaded'], on_click= functions.Change_play_State())
-
-    if not st.session_state["play_state"]:
+        st.write("Original Sound")
         st.audio(file_name)
- 
-#-------------------------------------------------------sliders---------------------------------------------------------------------------------------------------------------------------------------------
-
-# ----------------------------------------------------------------------fourier-------------------------------------------------------------------------------------------------------------------------------------------------------
-time_step=0.05
-time_vec=np.arange(0,10,time_step) #return evenly spaced time vector between [0,10]
-period=5 
-sig=(np.sin(2*np.pi*time_vec/period))+ 0.25*np.random.randn(time_vec.size) #for every 5*5 points, it completes a 1/4 cycle or every 20*5 points it completes 1 cycle +noise
-sig_fft=fftpack.fft(sig)
-amplitude=np.abs(sig_fft)
-power=amplitude**2
-phase=np.angle(sig_fft) #np.angle() return the angle of the complex argument
-sample_freq=fftpack.fftfreq(sig.size, d=time_step) #return the discrete fourier transform sample frequencies
-amp_freq=np.array([amplitude,sample_freq])
-amp_position= amp_freq[0,:].argmax()
-peak_freq = amp_freq[1, amp_position]
-high_freq_fft = sig_fft.copy()
-high_freq_fft[np.abs(sample_freq)> peak_freq]=0
-filtered_Sig=fftpack.ifft(high_freq_fft) # return discrete inverse fourier transform of real or complex sequence
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# # Filter the beat at 3kHz
-# filtered = uploaded_file.low_pass_filter(3000)
-# # Mix our filtered beat with the new loop at -3dB
-# final = filtered.overlay(loop2 - 3, loop=True)
-
-
- 
-#-------------------------------------------------------save--------------------------------------------------------------------------------------------------------
+        
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    fft_sig, amplitude,phase,sample_frequency=functions.Fourier_transform(data,samplerate)
+    List_freq_axis, List_amplitude_axis,bin_max_frequency_value=functions.bins_separation(sample_frequency, amplitude)
+    st.write(bin_max_frequency_value)
+    sliders_data=functions.generate_sliders(bin_max_frequency_value)
+    functions.show_signal(time,data) #plots wav file data in time domain
+    functions.plot_spectrogram(data,samplerate)
--- functions.py
+++ functions.py
@@ -5,20 +5,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sc
 from scipy.fftpack import fft
+from scipy.signal import find_peaks
 import  streamlit_vertical_slider  as svs
-import itertools
+import librosa
+import librosa.display
 
-
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def Change_play_State():
     st.session_state['play_state']=  not st.session_state['play_state']
 
+#-----------------------------------------------------------get Fmax----------------------------------------------------------------
+def getFMax(xAxis,yAxis):
+    amplitude = np.abs(fft.rfft(yAxis))
+    frequency = fft.rfftfreq(len(xAxis), (xAxis[1]-xAxis[0]))
+    indices = find_peaks(amplitude)
+    if len(indices[0])>0 :
+        max_freq=round(frequency[indices[0][-1]])
+    else:
+        max_freq=1   
+    return max_freq
 
-# def Audio_player(file):
-#     st.audio(file, format="audio/wav", start_time=0)
-
-
-# Read the Audiofile
+#-----------------------------------------------------------------------Read the Audiofile-----------------------------------------------------------------------------------------------------------------------------
+ 
 def handle_uploaded_audio_file(uploaded_file):
     a = AudioSegment.from_wav(uploaded_file)
     samples = a.get_array_of_samples()
@@ -26,14 +34,14 @@ def handle_uploaded_audio_file(uploaded_file):
     fp_arr /= np.iinfo(samples.typecode).max
     
     return fp_arr,  48000 #22050 
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def show_signal(x_axis,y_axis):
     SignalFigure, SignalAxis = plt.subplots(1, 1)
     SignalAxis.plot(x_axis,y_axis)
     plt.xlabel('Time [s]')
     plt.ylabel('Amplitude')
     st.plotly_chart(SignalFigure,use_container_width=True)
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # get the fourier transform of the file
 def Fourier_transform(data, samplerate):
     time_step=1/samplerate
@@ -43,20 +51,20 @@ def Fourier_transform(data, samplerate):
     sample_frequency =sc.fft.rfftfreq(len(data),d=time_step)  #return the discrete fourier transform sample frequencies
     return fft_sig, amplitude,phase,sample_frequency
 
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def bins_separation(frequency, amplitude):
     freq_axis_list = []
     amplitude_axis_list = []
-    bin_max_frequency_value = int(len(frequency)/10)
+    bin_max_frequency_value = int(len(frequency)/10) # 50 60 70 80 90 100 120  len()=20  int(20/10)=2  
     i = 0
     while(i < 10):
         freq_axis_list.append(
             frequency[i*bin_max_frequency_value : (i+1)*bin_max_frequency_value])
         amplitude_axis_list.append(
-            amplitude[i*bin_max_frequency_value:(i+1)*bin_max_frequency_value])
+            amplitude[i*bin_max_frequency_value : (i+1)*bin_max_frequency_value])
         i = i+1
     return freq_axis_list, amplitude_axis_list,bin_max_frequency_value
-
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # geberate sliders based on freq of uploaded file
 def generate_sliders(bin_max_frequency_value):
         min_value=0
@@ -71,25 +79,24 @@ def generate_sliders(bin_max_frequency_value):
                 min_value = 1- boundary
                 max_value = 1 + boundary
                 var = (i+1)*bin_max_frequency_value
-                slider=svs.vertical_slider(key=f"slider{i}", default_value=1, step=1, min_value=min_value, max_value=max_value)
+                slider1=svs.vertical_slider(key=i, default_value=1, step=1, min_value=min_value, max_value=max_value)
                 st.write(f" { var } HZ")
                 if var == None:
                     var = 1
-                sliders_data.append(slider)
+                sliders_data.append(slider1)
         return sliders_data
 
-# def sound_modification(sliders_data,amplitude_axis_list):
-#     empty = st.empty()
-#     empty.empty()
-#     modified_bins=[]
-#     for i in range(0,10):
-#         modified_bins.append( 10**(sliders_data[i]/20) * amplitude_axis_list[i])
-#     st.write(modified_bins)
-#     st.write(sliders_data)
-#     mod_amplitude_axis_list=list(itertools.chain.from_iterable(modified_bins))
-#     return mod_amplitude_axis_list,empty
+#-----------------------------------------------------------------spectrogram-----------------------------------------------------------------------------------------------------------------------------------
+def plot_spectrogram(data,samplerate):
+    FRAME_SIZE = 2048
+    HOP_SIZE = 512
+    S_scale = librosa.stft(data, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+    Y_scale = np.abs(S_scale) ** 2
+    Y_log_scale = librosa.power_to_db(Y_scale)
+    plt.figure(figsize=(25, 10))
+    librosa.display.specshow(Y_log_scale, 
+                             sr=samplerate, 
+                             hop_length=HOP_SIZE, 
+                             x_axis="time")
+    plt.colorbar(format="%+2.f")
 
-# def inverse_fourier(mod_amplitude_axis_list,phase):
-#     amplitude_modified=np.multiply(mod_amplitude_axis_list,np.exp(1j*phase))
-#     ifft_file=sc.ifft(amplitude_modified)
-#     return ifft_file
